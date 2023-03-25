17bfcf29129cd14760bab9dd6b5801cced184c9a
Great Joycs <pavigreatjoycs@gmail.com>
2023-03-25 11:14:32 +0530

    Added MyWorld

---
+++ src/main/java/com/luv2code/pkg/MyWorld.java
@@ -0,0 +1,11 @@
+package com.luv2code.pkg;
+
+public class MyWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello World");
+
+	}
+
+}
